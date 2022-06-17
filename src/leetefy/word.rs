e005e3c14c5b9f74d41149c8ccd7e660b0d670b7
zkud <anton.kudyrka@gmail.com>
2022-06-17 07:13:14 +0300

    Addes some unit-tests

--- src/leetefy/word.rs
+++ src/leetefy/word.rs
@@ -1,9 +1,38 @@
-pub fn leetefy_word(word: &String) -> String {
-  match word.as_str() {
+pub fn leetefy_word(word: &str) -> String {
+  match word {
+    "own" | "gain" => String::from("pwn"),
+    "owned" | "gained" => String::from("pwn'd"),
+    "owning" | "gaining" => String::from("pwng"),
     "soft" | "software" => String::from("warez"),
     "hacker" => String::from("hackxor"),
     "newbie" => String::from("noob"),
     "porn" | "pornography" => String::from("pron"),
-    _ => word.clone(),
+    _ => String::from(word),
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::leetefy_word;
+
+  #[test]
+  fn it_translates_to_dialect() {
+    let translations = [
+      ("own", "pwn"),
+      ("gain", "pwn"),
+      ("owned", "pwn'd"),
+      ("gained", "pwn'd"),
+      ("soft", "warez"),
+      ("software", "warez"),
+      ("hacker", "hackxor"),
+      ("newbie", "noob"),
+      ("porn", "pron"),
+      ("pornography", "pron"),
+      ("other", "other"),
+    ];
+
+    for (word, dialect) in translations {
+      assert_eq!(leetefy_word(word), dialect);
+    } 
   }
 }
